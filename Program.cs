e8b0db435255c059c3bbeb5c8d1cebf4b34fc203
Sebastián Webb <swebb1732@gmail.com>
2024-10-31 13:24:36 -0600

    Inicializar proyecto Examen1SO con estructura de aplicación de consola

--- Program.cs
+++ Program.cs
@@ -2,15 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Examen1SO
 {
-    internal class Program
+    class Program
     {
-        private static int num = 10;
+        static object lockBinario = new object();
+        static object lockOctal = new object();
+        static object lockHexadecimal = new object();
+
         static void Main(string[] args)
         {
+            int numero = 100; 
+
+            Thread hiloBinario = new Thread(() => ConvertirABinario(numero));
+            Thread hiloOctal = new Thread(() => ConvertirAOctal(numero));
+            Thread hiloHexadecimal = new Thread(() => ConvertirAHexadecimal(numero));
+
+            hiloBinario.Start();
+            hiloOctal.Start();
+            hiloHexadecimal.Start();
+
+            hiloBinario.Join();
+            hiloOctal.Join();
+            hiloHexadecimal.Join();
+
+            Console.WriteLine("Conversión completa. Presione una tecla para salir.");
+            Console.ReadKey();
+        }
+
+        static void ConvertirABinario(int numero)
+        {
+            lock (lockBinario)
+            {
+                lock (lockHexadecimal) 
+                {
+                    Console.WriteLine("Convirtiendo a Binario...");
+                    Thread.Sleep(5000);
+                    Console.WriteLine($"Binario: {Convert.ToString(numero, 2)}");
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        static void ConvertirAOctal(int numero)
+        {
+            lock (lockOctal)
+            {
+                lock (lockBinario) 
+                {
+                    Console.WriteLine("Convirtiendo a Octal...");
+                    Thread.Sleep(5000);
+                    Console.WriteLine($"Octal: {Convert.ToString(numero, 8)}");
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        static void ConvertirAHexadecimal(int numero)
+        {
+            lock (lockHexadecimal)
+            {
+                lock (lockOctal) 
+                {
+                    Console.WriteLine("Convirtiendo a Hexadecimal...");
+                    Thread.Sleep(5000);
+                    Console.WriteLine($"Hexadecimal: {Convert.ToString(numero, 16)}");
+                    Console.WriteLine("");
+                }
+            }
         }
     }
 }
